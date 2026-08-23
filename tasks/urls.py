2357f00b3936722f7eeaab94cf5894d9e1ae6847
Shahmeer0234 <ali.shahmeer2651@gmail.com>
2026-08-23 12:22:57 +0500

    Clean up scattered project files

--- tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.task_list_view, name='task_list'),
-    path('add/', views.add_task_view, name='add_task'),
-    path('complete/<int:task_id>/', views.mark_complete_view, name='mark_complete'),
-]
